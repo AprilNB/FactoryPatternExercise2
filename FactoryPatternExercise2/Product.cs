edb072d623b2a84a1994a9c5e3d1a5e32b5b377d
April Bullard <aprilnb86@gmail.com>
2021-06-30 12:13:14 -0500

    initial commit

---
+++ FactoryPatternExercise2/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPatternExercise2
+{
+    public class Product
+    {
+        public int Price { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
